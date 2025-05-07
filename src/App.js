6534101f8d0b30e0e8f2e05550bd4bda673148d0
River10321 <rivelinonf@icloud.com>
2025-05-07 20:01:49 -0300

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Produção Customizada</h1>
+      <p>Bem-vindo ao sistema. Em breve, você poderá cadastrar produtos, processos e funcionários!</p>
+    </div>
+  );
+}
+
+export default App;
