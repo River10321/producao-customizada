ddaa02d258464c6da9f01d3712dd60d7fa5305bc
River10321 <rivelinonf@icloud.com>
2025-05-08 10:52:05 -0300

    Update ListaProcessos.js

--- src/components/ListaProcessos.js
+++ src/components/ListaProcessos.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import { Paper, Typography, List, ListItem, ListItemText } from '@mui/material';
 
-const ListaFuncionarios = ({ funcionarios }) => {
+const ListaProcessos = ({ processos }) => {
   return (
     <Paper elevation={2} sx={{ p: 3, mt: 4 }}>
       <Typography variant="h6" gutterBottom>
-        Funcionários Cadastrados
+        Lista de Processos Cadastrados
       </Typography>
-      {funcionarios.length === 0 ? (
-        <Typography>Nenhum funcionário cadastrado.</Typography>
+      {processos.length === 0 ? (
+        <Typography>Nenhum processo cadastrado.</Typography>
       ) : (
         <List>
-          {funcionarios.map((f, index) => (
+          {processos.map((proc, index) => (
             <ListItem key={index} divider>
               <ListItemText
-                primary={`${f.nome} - R$ ${f.salario.toFixed(2)} | ${f.jornada}h/mês`}
-                secondary={`Custo por min: R$ ${(f.salario / (f.jornada * 60)).toFixed(4)} | por seg: R$ ${(f.salario / (f.jornada * 60 * 60)).toFixed(6)}`}
+                primary={`${proc.nome} - ${proc.min}min ${proc.seg}s`}
+                secondary={`Máquina: ${proc.maquina} | Produto: ${proc.produto}`}
               />
             </ListItem>
           ))}
@@ -25,4 +25,4 @@ const ListaFuncionarios = ({ funcionarios }) => {
   );
 };
 
-export default ListaFuncionarios;
+export default ListaProcessos;
