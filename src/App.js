e45d0eccf2f60c76c889c3cc2a3dec568bda8b3e
River10321 <rivelinonf@icloud.com>
2025-05-07 21:30:41 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import FormProcesso from './components/FormProcesso';
 import FormFuncionario from './components/FormFuncionario';
 import FormProduto from './components/FormProduto';
+import ListaProcessos from './components/ListaProcessos';
+import ListaFuncionarios from './components/ListaFuncionarios';
 
 function App() {
+  const [processos, setProcessos] = useState([]);
+  const [funcionarios, setFuncionarios] = useState([]);
+
+  const adicionarProcesso = (novo) => {
+    const tempo = novo.tempo;
+    const min = Math.floor(tempo / 60);
+    const seg = tempo % 60;
+    setProcessos([...processos, { ...novo, min, seg }]);
+  };
+
+  const adicionarFuncionario = (f) => {
+    setFuncionarios([...funcionarios, f]);
+  };
+
   return (
     <div className="App">
-      <FormProcesso />
-      <FormFuncionario />
+      <FormProcesso onAdd={adicionarProcesso} />
+      <ListaProcessos processos={processos} />
+      <FormFuncionario onAdd={adicionarFuncionario} />
+      <ListaFuncionarios funcionarios={funcionarios} />
       <FormProduto />
     </div>
   );
