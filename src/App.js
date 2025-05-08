924c915f61ce38e492021f1492b2e1a7341802af
River10321 <rivelinonf@icloud.com>
2025-05-07 21:19:33 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import FormProcesso from './components/FormProcesso';
 import FormFuncionario from './components/FormFuncionario';
+import FormProduto from './components/FormProduto';
 
 function App() {
   return (
     <div className="App">
       <FormProcesso />
       <FormFuncionario />
+      <FormProduto />
     </div>
   );
 }
